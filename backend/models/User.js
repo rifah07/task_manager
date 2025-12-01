b870080c872636c344e9dc5a716fa545690597ef
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-02 00:01:39 +0600

    user class added

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,2 +1,14 @@
 const bcrypt = require('bcrypt');
 const db = require('../config/database');
+
+class User {
+    constructor(data){
+        this.id = data.id;
+        this.username = data.username;
+        this.email = data.email;
+        this.password = data.password; // hashed password
+        this.role = data.role || 'user';
+    }
+}
+
+module.exports = User;
