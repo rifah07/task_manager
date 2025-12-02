3c9824bd5341864eeed1dae40be69a272f87a266
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-02 22:10:02 +0600

    password hashed

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,8 +6,17 @@ class User {
         this.id = data.id;
         this.username = data.username;
         this.email = data.email;
-        this.password = data.password; // hashed password
+        this.password = data.password;
         this.role = data.role || 'user';
+    }
+
+    // Encapsulation: Hide password hashing logic
+    async hashPassword(){
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+
+    async comparePassword(candidatePassword){
+        return await bcrypt.compare(candidatePassword, this.password);
     }
 }
 
