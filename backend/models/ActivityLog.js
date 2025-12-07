e2f118aea01185b23b29718bb1331e278edad5ab
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-07 12:06:07 +0600

    FUNCTION YO CREATE ACTIVITY LOG ADDED

--- backend/models/ActivityLog.js
+++ backend/models/ActivityLog.js
@@ -1,7 +1,12 @@
 const db = require('../config/database');
 
 class ActivityLog {
-
+    static async create(userId, taskId, action, details){
+        return await db.query(
+            `INSERT INTO activity_logs (userId, taskId, action, details) VALUES (?, ?, ?, ?)`,
+            [userId, taskId, action, details]
+        );
+    }
 }
 
 module.exports = ActivityLog;
