6c2c71576532d91123b821e032f71d3ce5841a6a
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-09 15:12:56 +0600

    activity log is completed

--- backend/models/ActivityLog.js
+++ backend/models/ActivityLog.js
@@ -1,16 +1,22 @@
 const db = require('../config/database');
 
 class ActivityLog {
-    static async create(userId, taskId, action, details){
+    static async create(userId, taskId, action, details) {
         return await db.query(
             `INSERT INTO activity_logs (userId, taskId, action, details) VALUES (?, ?, ?, ?)`,
             [userId, taskId, action, details]
         );
     }
-    static async getByTaskId(taskId){
+    static async getByTaskId(taskId) {
         return await db.query(
             'SELECT al.*, u.username FROM activity_logs al JOIN users u ON al.userId = u.id WHERE task_id = ? ORDER BY created_at DESC',
             [taskId]
+        );
+    }
+    static async getByUserId(userId) {
+        return await db.query(
+            'SELECT * FROM activity_logs WHERE user_id = ? ORDER BY created_at DESC LIMIT 50',
+            [userId]
         );
     }
 }
