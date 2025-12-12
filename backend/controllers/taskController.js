e13a5b3e23ed48a7c20e728907f9d5c31e1c97db
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-12 19:36:28 +0600

    task controller created, initialized functions yet to implement

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,19 @@
+const Task = require('../models/Task');
+const ActivityLog = require('../models/ActivityLog');
+
+class TaskController {
+    static async createTask(req, res) { }
+
+    static async getTasks(req, res) { }
+
+    static async getTaskById(req, res) { }
+
+    static async updateTask(req, res) { }
+
+    static async deleteTask(req, res) { }
+
+    static async getActivityLogs(req, res) { }
+
+}
+
+module.exports = TaskController;
