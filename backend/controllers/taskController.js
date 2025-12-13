a63a83d7c04cbd39f5717bba23aaefb0a7748d7d
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-12-13 08:04:30 +0600

    nwe task creation function added

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -2,17 +2,57 @@ const Task = require('../models/Task');
 const ActivityLog = require('../models/ActivityLog');
 
 class TaskController {
-    static async createTask(req, res) { }
+    static async createTask(req, res) {
+        try {
+            const taskData = { ...req.body, created_by: req.user.id};
+            const task = new Task(taskData);
+            await task.save();
+            
+            res.status(201).json(task)
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
 
-    static async getTasks(req, res) { }
+    static async getTasks(req, res) {
+        try {
 
-    static async getTaskById(req, res) { }
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
 
-    static async updateTask(req, res) { }
+    static async getTaskById(req, res) {
+        try {
 
-    static async deleteTask(req, res) { }
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
 
-    static async getActivityLogs(req, res) { }
+    static async updateTask(req, res) {
+        try {
+
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async deleteTask(req, res) {
+        try {
+
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async getActivityLogs(req, res) {
+        try {
+
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
 
 }
 
